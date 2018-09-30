9b3dffdea1409b3ba0af259ef7240559d41c7115
omedale <omedale@gmail.com>
2018-09-30 07:21:44 +0100

    Feat/setup UI(view)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,26 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+//= require dropzone
+
+$(document).ready(function(){
+	// disable auto discover
+	Dropzone.autoDiscover = false;
+
+	// grap our upload form by its id
+	$("#new_upload").dropzone({
+		// restrict image size to a maximum 1MB
+		maxFilesize: 1,
+		// changed the passed param to one accepted by
+		// our rails app
+		paramName: "upload[image]",
+		// show remove links on each image upload
+		addRemoveLinks: true
+	});	
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'uploads/create'
+  root 'file#index'
+
+  resources 'uploads'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
