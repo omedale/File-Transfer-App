682f06d734504fd01deabf28612cb69d24f7723c
omedale <omedale@gmail.com>
2018-09-30 14:40:28 +0100

    add progress bar, validations  and fix cors

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,22 @@
 class Upload < ApplicationRecord
   has_many_attached :files
+  validates :title, presence: true
+  validates :description, presence: true
+  validate :validate_file
+
+  private
+
+  def validate_file
+    if files.attached? == false
+      files.purge_later
+      errors[:base] << 'File is not attached'
+    elsif files.attached?
+      files.each do |file|
+        if file.byte_size > 10485760
+          file.purge
+          errors[:base] << 'File is too large. It has to be below 10MB'
+        end
+      end
+    end
+  end
 end
