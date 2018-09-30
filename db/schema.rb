4daa12743d84ca135ddab14ff27dd2f7b8d1104d
omedale <omedale@gmail.com>
2018-09-30 21:25:36 +0100

    secure file download

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_085029) do
+ActiveRecord::Schema.define(version: 2018_09_30_182015) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2018_09_30_085029) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "passphrase"
   end
 
 end
